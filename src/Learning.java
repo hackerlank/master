219696d7fb3d687db2d5f3910d82bbdc78bb9e41
Arthur Lukashchuk <Arthur Lukashchuk>
2016-04-30 22:12:00 +0300

    Learning

---
+++ src/Learning.java
@@ -0,0 +1,15 @@
+public class Learning {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//dog=Shaava cat=Masia dog=Fatty і виведе назад в консоль dog=Shaava,Fatty cat=Masia
+		String dogs = "Shaava, Fatty";
+		String cat = "Masia";
+		System.out.println("dogs=" +dogs);
+		System.out.println("cat=" +cat);
+	}
+
+}
